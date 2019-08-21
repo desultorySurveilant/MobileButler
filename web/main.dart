eaaff9b52abe710814c6291849e9e788169e765e
desultorySurveilant <antonio.marques@sbcglobal.net>
2019-08-21 17:55:50 -0500

    Now basically functional

--- web/main.dart
+++ web/main.dart
@@ -1,41 +1,50 @@
 import 'dart:html';
 
-import 'dart:svg';
-
 void main() {
   Element output = querySelector('#output');
-  TextInputElement handle = TextInputElement()..id = 'handle';
+  TextAreaElement handle = TextAreaElement()..id = 'handle';
   output.append(handle);
-  TextInputElement message = TextInputElement()..id = 'message';
+  output.appendHtml('</br>');
+  TextAreaElement message = TextAreaElement()..id = 'message';
   output.append(message);
-  TextElement url = TextElement();
-  ButtonElement submit = ButtonElement()..id = 'submit';
+  output.appendHtml('</br>');
+  TextAreaElement url = TextAreaElement()..id = 'url' ..value = 'url will appear here.';
+  ButtonElement submit = ButtonElement()..id = 'submit'..text = 'Generate url';
   submit.onClick.listen((e){
-    url.text = 'http://farragofiction.com:8500/TalkButlerBot?chatHandle=${pr(handle.value)}&input=${pr(message.value)}';
+    url.value = 'http://farragofiction.com:8500/TalkButlerBot?chatHandle=${pr(handle.value)}&input=${pr(message.value)}';
     });
   output.append(submit);
+  output.appendHtml('</br>');
   output.append(url);
+  handle.onChange.listen((e){
+    url.value = 'http://farragofiction.com:8500/TalkButlerBot?chatHandle=${pr(handle.value)}&input=${pr(message.value)}';
+  });
+  message.onChange.listen((e){
+    url.value = 'http://farragofiction.com:8500/TalkButlerBot?chatHandle=${pr(handle.value)}&input=${pr(message.value)}';
+  });
 }
-String pr(String str){
-  str.replaceAll(' ', '+');
-  str.replaceAll('!', '%21');
-  str.replaceAll('#', '%23');
-  str.replaceAll('\$', '%24');
-  str.replaceAll('%', '%25');
-  str.replaceAll('&', '%26');
-  str.replaceAll('\'', '%27');
-  str.replaceAll('(', '%28');
-  str.replaceAll(')', '%29');
-  str.replaceAll('*', '%2A');
-  str.replaceAll('+', '%2B');
-  str.replaceAll(',', '%2C');
-  str.replaceAll('/', '%2F');
-  str.replaceAll(':', '%3A');
-  str.replaceAll(';', '%3B');
-  str.replaceAll('=', '%3D');
-  str.replaceAll('?', '%3F');
-  str.replaceAll('@', '%40');
-  str.replaceAll('[', '%5B');
-  str.replaceAll(']', '%5D');
+String pr(String input){
+  var str = input;
+  print('Replacing!');
+  str = str.replaceAll('%', '%25');
+  str = str.replaceAll(' ', '+');
+  str = str.replaceAll('!', '%21');
+  str = str.replaceAll('#', '%23');
+  str = str.replaceAll('\$', '%24');
+  str = str.replaceAll('&', '%26');
+  str = str.replaceAll('\'', '%27');
+  str = str.replaceAll('(', '%28');
+  str = str.replaceAll(')', '%29');
+  str = str.replaceAll('*', '%2A');
+  str = str.replaceAll('+', '%2B');
+  str = str.replaceAll(',', '%2C');
+  str = str.replaceAll('/', '%2F');
+  str = str.replaceAll(':', '%3A');
+  str = str.replaceAll(';', '%3B');
+  str = str.replaceAll('=', '%3D');
+  str = str.replaceAll('?', '%3F');
+  str = str.replaceAll('@', '%40');
+  str = str.replaceAll('[', '%5B');
+  str = str.replaceAll(']', '%5D');
   return str;
 }
